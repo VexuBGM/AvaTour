67085d2163028baad78b7366cccb1f5efcd3bf2a
VexuBGM <emoniksp@gmail.com>
2025-01-21 22:22:23 +0200

    chore: env for backend

--- backend/avaserver/settings.py
+++ backend/avaserver/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+current_dir = Path(__file__).resolve().parent
+env_file = current_dir / '.env'
+load_dotenv(dotenv_path=env_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'AvaDB',
         'USER': 'root',
-        'PASSWORD': '17052008',
+        'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '3306',
     }
