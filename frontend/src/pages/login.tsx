a0c3484579c6d2e731a3117c02bb6ef13f235aeb
KonstantinKalev <constantinekalev27@itpg-varna.bg>
2025-01-22 23:48:46 +0200

    изравняване на файловете с Емо

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import '../styles/globals.css';
+import '../app/globals.css';
 
 export default function Login() {
   const [formData, setFormData] = useState({
