229f7be8af7cf6d6a2f28fc25aa0280a0e6e2558
VexuBGM <emoniksp@gmail.com>
2025-01-22 23:52:10 +0200

    test

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import '../app/globals.css';
+import '../app/globals.css'; // TESTTTT
 
 export default function Login() {
   const [formData, setFormData] = useState({
