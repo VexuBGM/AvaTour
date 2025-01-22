82bf7a01a4328c7f5d0688f79461c21805bd2c38
KonstantinKalev <constantinekalev27@itpg-varna.bg>
2025-01-22 23:57:30 +0200

    done experimenting

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import '../app/globals.css'; // TESTTTT
-
+import '../app/globals.css';
 export default function Login() {
   const [formData, setFormData] = useState({
     username: '',
