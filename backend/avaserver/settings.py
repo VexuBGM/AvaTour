687778aea874abdbae7748b24a0f33b664c72fb0
VexuBGM <emoniksp@gmail.com>
2025-03-03 19:30:57 +0200

    fix: ngrok

--- backend/avaserver/settings.py
+++ backend/avaserver/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["167.172.160.212"]
+ALLOWED_HOSTS = ["167.172.160.212", "5787-167-172-160-212.ngrok-free.app"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -3,6 +3,6 @@
 // export const shortApi = 'http://localhost:8000';
 
 // Production
-export const api = 'https://167.172.160.212:8000/api';
-export const csrfTokenUrl = 'https://167.172.160.212:8000/csrf-token';
-export const shortApi = 'https://167.172.160.212:8000';
+export const api = 'https://5787-167-172-160-212.ngrok-free.app/api';
+export const csrfTokenUrl = 'https://5787-167-172-160-212.ngrok-free.app/csrf-token';
+export const shortApi = 'https://5787-167-172-160-212.ngrok-free.app/';
