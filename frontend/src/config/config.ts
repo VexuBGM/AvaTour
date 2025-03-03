cc4a042f0882fba68ba2a594e39af7e1c1e29cbd
VexuBGM <emoniksp@gmail.com>
2025-03-03 20:06:59 +0200

    fix: link ngrok

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -3,6 +3,6 @@
 // export const shortApi = 'http://localhost:8000';
 
 // Production
-export const api = 'https://5787-167-172-160-212.ngrok-free.app/api';
-export const csrfTokenUrl = 'https://5787-167-172-160-212.ngrok-free.app/csrf-token';
-export const shortApi = 'https://5787-167-172-160-212.ngrok-free.app/';
+export const api = 'https://02b9-167-172-160-212.ngrok-free.app/api';
+export const csrfTokenUrl = 'https://02b9-167-172-160-212.ngrok-free.app/csrf-token';
+export const shortApi = 'https://02b9-167-172-160-212.ngrok-free.app/';
