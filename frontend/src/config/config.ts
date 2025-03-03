4b21351c43937c372bcef454044ad0aec29643e6
VexuBGM <emoniksp@gmail.com>
2025-03-03 18:21:13 +0200

    prod: links

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,3 +1,8 @@
-export const api = 'http://localhost:8000/api';
-export const csrfTokenUrl = 'http://localhost:8000/csrf-token/';
-export const shortApi = 'http://localhost:8000';
+// export const api = 'http://localhost:8000/api';
+// export const csrfTokenUrl = 'http://localhost:8000/csrf-token/';
+// export const shortApi = 'http://localhost:8000';
+
+// Production
+export const api = 'http://167.172.160.212:8000/api';
+export const csrfTokenUrl = 'http://167.172.160.212:8000/csrf-token/';
+export const shortApi = 'http://167.172.160.212:8000';
