89439673f35d41e7db2b7d09d4decbf7c16de214
VexuBGM <emoniksp@gmail.com>
2025-03-07 00:39:15 +0200

    fix: Deployment

--- backend/avaserver/settings.py
+++ backend/avaserver/settings.py
@@ -33,7 +33,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware', 
+    'django.middleware.csrf.CsrfViewMiddleware', 
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -108,13 +108,13 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-# CSRF_TRUSTED_ORIGINS = [
-#     "http://localhost:3000",
-#     "https://avatour-alpha.vercel.app",
-#     "https://avatour.website",
-#     "https://www.avatour.website",
-#     "https://api.avatour.website",
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "https://avatour-alpha.vercel.app",
+    "https://avatour.website",
+    "https://www.avatour.website",
+    "https://api.avatour.website",
+]
 
 CSRF_COOKIE_SECURE = False
 CSRF_USE_SESSIONS = False
