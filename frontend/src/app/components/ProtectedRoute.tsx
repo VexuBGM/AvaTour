103b23b6109f9e1a3152926e9dd28f14b759e198
VexuBGM <emoniksp@gmail.com>
2025-03-07 00:43:44 +0200

    fix: Deployment

--- frontend/src/app/components/ProtectedRoute.tsx
+++ frontend/src/app/components/ProtectedRoute.tsx
@@ -24,7 +24,7 @@ axios.interceptors.request.use((config) => {
 function getCookie(name: string) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+  if (parts.length === 2) return parts.pop()!.split(';').shift();
 }
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
