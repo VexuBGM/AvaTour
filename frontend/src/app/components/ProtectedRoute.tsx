3515e568bbde7d12fb5944324dcbb5e95398701b
VexuBGM <emoniksp@gmail.com>
2025-03-07 00:48:12 +0200

    fix: Deployment

--- frontend/src/app/components/ProtectedRoute.tsx
+++ frontend/src/app/components/ProtectedRoute.tsx
@@ -6,27 +6,6 @@ import axios from 'axios';
 import { ReactNode } from 'react';
 import { api } from '@/config/config';
 
-axios.defaults.withCredentials = true;
-
-// Add request interceptor to set CSRF token
-axios.interceptors.request.use((config) => {
-  const csrfToken = getCookie('csrftoken');  // Function to get csrf token from cookies
-
-  if (csrfToken) {
-    config.headers['X-CSRFToken'] = csrfToken;  // Set CSRF token in the header
-  }
-
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
-
-function getCookie(name: string) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()!.split(';').shift();
-}
-
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   const router = useRouter();
